1ccc0e84df72ac812d217e8c099b30ace0cdfc28
Diogo Faria <diogofaria073@gmail.com>
2020-03-17 15:00:13 -0300

    Add esconfigs

--- gobarber.api/src/app.js
+++ gobarber.api/src/app.js
@@ -3,7 +3,6 @@ import routes from './routes';
 
 class App {
   constructor() {
-
     this.server = express();
 
     this.middlewares();
--- gobarber.api/src/routes.js
+++ gobarber.api/src/routes.js
@@ -2,9 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'My First API is running' })
-})
-
+routes.get('/', (req, res) => res.json({ message: 'My First API is running' }));
 
 export default routes;
