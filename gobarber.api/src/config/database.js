5cb94f1d88e5d4d0b284d354d84d40614aba2ffa
Diogo Faria <diogofaria073@gmail.com>
2020-03-17 20:33:24 -0300

    Add database config files

--- gobarber.api/src/config/database.js
+++ gobarber.api/src/config/database.js
@@ -1,3 +1,11 @@
 module.exports = {
-  dialect: '',
+  dialect: 'postgres',
+  host: 'gobarberdb.cwh4nwo5p4o3.us-east-2.rds.amazonaws.com',
+  username: 'postgres',
+  password: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
 };
