8cb70d28ba005b99d23cddaf377effc086f76c6f
Diogo Faria <diogofaria073@gmail.com>
2020-03-24 12:55:12 -0300

    filter to list appointments

--- gobarber.api/src/app/controllers/AppointmentController.js
+++ gobarber.api/src/app/controllers/AppointmentController.js
@@ -6,10 +6,13 @@ import Appointment from '../models/Appointment';
 
 class AppointmentControler {
   async index(req, res) {
+    const { page = 1 } = req.query;
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       orderby: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
--- gobarber.api/src/routes.js
+++ gobarber.api/src/routes.js
@@ -9,6 +9,7 @@ import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,6 +17,7 @@ const upload = multer(multerConfig);
 routes.post('/sessions', SessionControler.store);
 
 routes.put('/users', authMiddleware, UserController.update);
+
 routes.post('/users', UserController.store);
 
 routes.get('/providers', ProviderController.index);
@@ -25,5 +27,7 @@ routes.post('/files', upload.single('file'), FileController.store);
 routes.post('/appointments', authMiddleware, AppointmentController.store);
 
 routes.get('/appointments', authMiddleware, AppointmentController.index);
+
+routes.get('/schedules', authMiddleware, ScheduleController.index);
 
 export default routes;
