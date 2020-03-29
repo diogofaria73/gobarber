8de50fb2ec2fafeb733619d81b4e234b57f9855b
Diogo Faria <diogofaria073@gmail.com>
2020-03-29 14:32:06 -0300

    Available hours

--- gobarber.api/src/app/controllers/AvailableController.js
+++ gobarber.api/src/app/controllers/AvailableController.js
@@ -1,6 +1,57 @@
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
+import { Op } from 'sequelize';
+import schedule from '../utils/scheduleTime';
+
+import Appointments from '../models/Appointment';
+
 class AvailableController {
   async index(request, response) {
-    return response.json('OK');
+    const { date } = request.query;
+    const { id } = request.params;
+
+    console.log(id);
+
+    if (!date) {
+      return response.status(400).json({ error: 'Invalid date' });
+    }
+
+    const searchDate = Number(date);
+
+    const appointments = await Appointments.findAll({
+      where: {
+        provider_id: id,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    const available = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return response.json({ available });
   }
 }
 
