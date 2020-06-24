b8e4c26e86484fb995d2608daf6ff34222bf9df4
diogofaria73 <diogofaria073@gmail.com>
2020-06-23 21:25:00 -0300

    Ajustes

--- gobarber.front/teste.js
+++
@@ -1,5 +0,0 @@
-const hours = new Date().getHours();
-const minutes = new Date().getMinutes();
-const seconds = new Date().getSeconds();
-
-console.log(`Agora são: ${hours} : ${minutes} : ${seconds}`);
