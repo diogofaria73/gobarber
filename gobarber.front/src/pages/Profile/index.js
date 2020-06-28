c16d453b5584082536a3103a79f2fd9db0ab43a7
diogofaria73 <diogofaria073@gmail.com>
2020-06-28 14:56:45 -0300

    Add user reducer

--- gobarber.front/src/pages/Profile/index.js
+++ gobarber.front/src/pages/Profile/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
+import { updateProfileRequest } from '~/store/modules/user/actions';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-function Profile() {
-  return <h1>Profile Page</h1>;
+export default function Profile() {
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.user.profile);
+  // eslint-disable-next-line no-console
+  console.log(profile);
+
+  function handleSubmit(data) {
+    // console.tron.log(data);
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+        <hr />
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Sua nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua nova senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+      <button type="button">Sair da Aplicação</button>
+    </Container>
+  );
 }
-
-export default Profile;
